7b1c67c2c0547c86c2c6a992cf18a60e1e722bb1
jyajoo <jyajoo1020@gmail.com>
2023-08-01 14:25:08 +0900

    style: Login 페이지

--- bookreport-fe/src/routes/Login.js
+++ bookreport-fe/src/routes/Login.js
@@ -1,50 +1,100 @@
 import React from "react";
+
 import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-} from "mdb-react-ui-kit";
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+import Layout from "../components/Layout"
+import customAxios from "../api/customAxios.js"
+import {icon, MixinToast} from "../components/Alert.js"
 
 function Login() {
-  return (
-    <MDBContainer fluid>
-      <MDBRow className="d-flex justify-content-center align-items-center h-100">
-        <MDBCol col="12">
-          <MDBCard
-            className="bg-white my-5 mx-auto"
-            style={{ borderRadius: "1rem", maxWidth: "500px" }}
-          >
-            <MDBCardBody className="p-5 w-100 d-flex flex-column">
-              <h2 className="fw-bold mb-2 text-center">로그인</h2>
-              <p className="text-white-50 mb-3">
-                아이디와 비밀번호를 입력해주세요.
-              </p>
-
-              <MDBInput
-                wrapperClass="mb-4 w-100"
-                label="아이디"
-                id="formControlLg"
-                type="text"
-                size="lg"
-              />
-              <MDBInput
-                wrapperClass="mb-4 w-100"
-                label="비밀번호"
-                id="formControlLg"
-                type="password"
-                size="lg"
-              />
-              <MDBBtn size="lg">Login</MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
+    return (
+      <Layout>
+          <section className="section section-shaped section-lg vh-100">
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="pt-lg-7">
+              <Row className="justify-content-center">
+                <Col lg="5">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardHeader className="bg-white">
+                      <div className="text-muted text-center">
+                        <small>Sign in with credentials</small>
+                      </div>
+                    </CardHeader>
+                    <CardBody className="px-lg-5 py-lg-5">
+                      <Form role="form">   
+                        <FormGroup className="mb-3">
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="fa fa-user" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="아이디" type="text" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              placeholder="비밀번호"
+                              type="password"
+                              autoComplete="off"
+                            />
+                          </InputGroup>
+                        </FormGroup>
+                        <div className="text-center">
+                          <Button
+                            className="my-4"
+                            color="primary"
+                            type="button"
+                          >
+                            로그인
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                    <div className="text-right" xs="6">
+                      <a
+                        className="text-light"
+                        href="/member/join"
+                      >
+                        <small>Create new account</small>
+                      </a>
+                    </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+      </Layout>
+    );
 }
 
 export default Login;
