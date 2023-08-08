e5f4d923d6c60915194ab3b250c49f4367e45544
jyajoo <jyajoo1020@gmail.com>
2023-08-08 18:29:18 +0900

    style: hero 영역 추가

--- bookreport-fe/src/App.js
+++ bookreport-fe/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./components/Layout"
+import Hero from "./components/Hero"
 import {CookiesProvider} from 'react-cookie';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <CookiesProvider>
       <Layout />
+      <Hero />
     </CookiesProvider>
 
   );
