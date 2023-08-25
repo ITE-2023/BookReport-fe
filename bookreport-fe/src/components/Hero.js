47b9d2e30e36936d68a74ff7788b90ebe2f0ca90
jyajoo <jyajoo1020@gmail.com>
2023-08-25 23:55:02 +0900

    feat: hero 수정

--- bookreport-fe/src/components/Hero.js
+++ bookreport-fe/src/components/Hero.js
@@ -1,53 +1,67 @@
 import React from "react";
 
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, UncontrolledCarousel } from "reactstrap";
+
+const items = [
+  {
+    src: require("../assets/img/LOGO.-removebg-preview1111.png"),
+    altText: "",
+    caption: "",
+    header: "",
+  },
+  {
+    src: require("../assets/img/LOGO.-removebg-preview1111.png"),
+    altText: "",
+    caption: "",
+    header: "",
+  },
+];
 
 function Hero() {
-    return (
-        <>
-        <div className="position-relative">
-            <section className="section section-hero section-shaped">
-            <div className="shape shape-style-1 shape-default bg-gradient-default">
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-            </div>
-            <Container className="shape-container d-flex align-items-center py-lg">
-                <div className="col px-0">
-                <Row className="align-items-center justify-content-center">
-                    <Col className="text-center" lg="6">
-                    <p className="lead text-white">
-                        A beautiful Design System for Bootstrap 4. It's Free and
-                        Open Source.
-                    </p>
-                    </Col>
-                </Row>
+  return (
+    <>
+      <div className="position-relative">
+        <section className="section section-hero section-shaped">
+          <div className="shape shape-style-1 shape-default">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <Container className="py-md-5">
+            <Row className="justify-content-between align-items-center">
+              <Col className="mb-5 mb-lg-0" lg="5">
+                <h1 className="text-white font-weight-light">Music & Book</h1>
+                <p className="lead text-white mt-4">
+                  감정을 쓰며, 음악을 만나 독서 경험을 공유하는 플랫폼
+                </p>
+              </Col>
+              <Col className="mb-lg-auto" lg="6">
+                <div className="rounded shadow-lg overflow-hidden transform-perspective-right">
+                  <UncontrolledCarousel items={items} />
                 </div>
-            </Container>
-            <div className="separator separator-bottom separator-skew zindex-100">
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-                >
-                <polygon
-                    className="fill-white"
-                    points="2560 0 2560 100 0 100"
-                />
-                </svg>
-            </div>
-            </section>
-        </div>
-        </>
-    );
+              </Col>
+            </Row>
+          </Container>
+          {/* SVG separator */}
+          <div className="separator separator-bottom separator-skew">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
+            </svg>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
--- bookreport-fe/src/routes/Home.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>메인 페이지</div>;
-}
-
-export default Home;
