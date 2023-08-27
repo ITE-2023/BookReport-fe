eb90debd05065e7788da8e2952af24d142f65053
jyajoo <jyajoo1020@gmail.com>
2023-08-27 18:30:06 +0900

    style: search 칸 추가

--- bookreport-fe/src/App.js
+++ bookreport-fe/src/App.js
@@ -1,16 +1,48 @@
-import React from "react";
-import Layout from "./components/Layout"
-import Hero from "./components/Hero"
-import {CookiesProvider} from 'react-cookie';
+import React, { useState } from "react";
+import Layout from "./components/Layout";
+import Hero from "./components/Hero";
+import { CookiesProvider } from "react-cookie";
+import classnames from "classnames";
+import {
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+} from "reactstrap";
+import "./css/App.css";
 
 function App() {
+  const [state, setState] = useState({});
 
   return (
     <CookiesProvider>
-      <Layout />
-      <Hero />
+      <Layout>
+        <Hero />
+        <section className="text-center">
+          <p className="lead">책을 검색하여 독후감과 음악을 기록해보세요!</p>
+          <FormGroup
+            className={classnames({
+              focused: state.searchAltFocused,
+            })}
+          >
+            <InputGroup className="input-group-alternative mb-4" id="search">
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>
+                  <i className="ni ni-zoom-split-in" />
+                </InputGroupText>
+              </InputGroupAddon>
+              <Input
+                placeholder="책 제목을 검색하세요"
+                type="text"
+                onFocus={(e) => setState({ searchAltFocused: true })}
+                onBlur={(e) => setState({ searchAltFocused: false })}
+              />
+            </InputGroup>
+          </FormGroup>
+        </section>
+      </Layout>
     </CookiesProvider>
-
   );
 }
 
