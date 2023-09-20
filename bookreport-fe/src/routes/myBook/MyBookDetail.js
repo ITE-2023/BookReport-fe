5bdcbe651115d7db7113046af99fce3b546eb7b1
jyajoo <jyajoo1020@gmail.com>
2023-09-20 22:45:05 +0900

    feat: MyBookDetail 페이지 추가

--- bookreport-fe/src/routes/myBook/MyBookDetail.js
+++ bookreport-fe/src/routes/myBook/MyBookDetail.js
@@ -1,7 +1,103 @@
 import Layout from "../../components/Layout";
+import Hero from "../../components/Hero";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Row,
+  Modal,
+  NavItem,
+  NavLink,
+  Nav,
+  CardHeader,
+  CardBody,
+  TabContent,
+  TabPane,
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+} from "reactstrap";
+import styles from "../../css/BookDetail.module.css";
+import { useState, useRef, useMemo, useEffect } from "react";
 
 function MyBookDetail() {
-  return <Layout></Layout>;
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [description, setDescription] = useState(
+    "잠들어야만 입장 가능한 꿈 백화점에서 일어나는 비밀스럽고도 기묘하며 가슴 뭉클한 판타지 소설\n\n여기는 잠들어야만 입장할 수 있는 ‘달러구트 꿈 백화점’입니다. 잠들어야만 입장할 수 있는 독특한 마을. 그곳에 들어온 잠든 손님들에게 가장 인기 있는 곳은, 온갖 꿈을 한데 모아 판매하는 ‘달러구트의 꿈 백화점’이다. 긴 잠을 자는 사람들은 물론이고, 짧은 낮잠을 자는 사람들과 동물들로 매일매일 대성황을 이룬다. \n범상치 않은 혈통의 주인장 ‘달러구트’, 그리고 그의 최측근에서 일하게 된 신참 직원 ‘페니’, 꿈을 만드는 제작자 ‘아가넵 코코’, 그리고 베일에 둘러싸인 비고 마이어스…등이 등장한다. \n《달러구트 꿈 백화점》은 ‘무의식에서만 존재하는 꿈을 정말 사고 팔 수 있을까?’라는 기발한 질문에 답을 찾아가며, 꿈을 만드는 사람, 파는 사람, 사는 사람의 비밀스런 에피소드를 담고 있는 판타지 소설이다. 텀블벅 펀딩 1812% 달성, 전자책 출간 즉시 베스트셀러 1위를 3주간 기록하며 수많은 독자들의 요청으로 종이책으로 출간하게 되었다."
+  );
+
+  const [isMore, setIsMore] = useState(false);
+  const descriptionLimit = useRef(200);
+  const commenter = useMemo(() => {
+    const shortDescription = description.slice(0, descriptionLimit.current);
+
+    if (description.length > descriptionLimit.current) {
+      if (isMore) {
+        return description;
+      }
+      return shortDescription + "...";
+    }
+    return description;
+  }, [isMore, description]);
+
+  return (
+    <Layout>
+      <Hero>
+        <Container className="mt-5">
+          <Card className={styles.bookDetailBox}>
+            <Row className="align-items-top">
+              <Col className="text-center">
+                <img
+                  className={styles.bookImage}
+                  src="https://shopping-phinf.pstatic.net/main_3243610/32436104744.20230905101559.jpg"
+                  alt="bookImage"
+                ></img>
+              </Col>
+              <Col className={styles.bookDetail} sm="7">
+                <p>
+                  <span className={styles.bookTitle}>
+                    달러구트 꿈 백화점 1 (주문하신 꿈은 매진입니다)
+                  </span>
+                  <span className="text-muted"> (9791165341909)</span>
+                </p>
+                <p className="text-muted">
+                  이미예 &nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;&nbsp;&nbsp;{" "}
+                  팩토리나인 (출판사)
+                </p>
+                <br />
+                <h6 className="font-weight-bold text-muted">책 소개</h6>
+                <p>{commenter}</p>
+                <div
+                  onClick={() => setIsMore(!isMore)}
+                  className={`text-muted ${styles.moreBtn}`}
+                >
+                  {description.length > descriptionLimit.current &&
+                    (isMore ? "[접기]" : "[더보기]")}
+                </div>
+              </Col>
+              <Col>
+                <div className="mb-3 text-center">
+                  <span>수정</span>&nbsp;/&nbsp;<span>삭제</span>
+                </div>
+                <p>평가 : ⭐⭐⭐⭐⭐</p>
+                <p>시작일 : 2023-09-20 </p>
+                <p>종료일 : 2023-09-25</p>
+                <p>독서량 : 100쪽</p>
+                <p>시작일 : 2023-09-20 </p>
+                <p>기대평 : 기대기대기대평 </p>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
+      </Hero>
+    </Layout>
+  );
 }
 
 export default MyBookDetail;
